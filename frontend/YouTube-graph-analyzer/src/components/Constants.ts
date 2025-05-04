e0a7744ffee76aa12c7cb2714afeb799a8504542
Aaron Duda <aaronduda1723@gmail.com>
2025-05-04 15:20:54 -0400

    Change to https

--- frontend/YouTube-graph-analyzer/src/components/Constants.ts
+++ frontend/YouTube-graph-analyzer/src/components/Constants.ts
@@ -1,4 +1,4 @@
 import dayjs from "dayjs";
 
-export const NEO4J_SERVER_ADDRESS = 'http://34.230.136.190:3000';
+export const NEO4J_SERVER_ADDRESS = 'https://34.230.136.190:433';
 export const YOUTUBE_DATE = dayjs('2007-02-15');
